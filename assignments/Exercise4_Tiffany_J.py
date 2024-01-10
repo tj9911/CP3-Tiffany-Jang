7bb76e9497c8da76eff1a8635dd40b590df8f2a3
Tiff <play.koyo@gmail.com>
2024-01-10 21:03:46 +0900

    Create Exercise4_Tiffany_J.py

---
+++ assignments/Exercise4_Tiffany_J.py
@@ -0,0 +1,21 @@
+# student info
+name = "Tiffany Jang"
+sdu_code="9911"
+
+## Subjects
+f = "Foundation English"
+g = "General Business"
+i = "Introduction to Computer Systems"
+c = "Computer Programming"
+
+### Final points
+print("============  คะแนนสอบวืชาฝึกหัด  =============")
+print("Student Name : " + name)
+print("Student Code : " + sdu_code)
+print("--------------- Your Score -----------------")
+print(f , ":" , 20+45+60+80/100 , "คะแนน")
+print(g , ":" , 20+45+60+73/100 , "คะแนน")
+print(c , ":" , 20+45+60+60/100 , "คะแนน")
+print(i , ":" , 20+45+60+50/100 , "คะแนน")
+
+print("--------------------------------------------")
